7e3e0953b8da9547be1f4920af718b320a237420
Kevin Volkel <kevinator900@gmail.com>
2021-12-13 11:13:41 -0700

    started day12

--- day12/script.js
+++ day12/script.js
@@ -33,25 +33,9 @@ b-end`
 input = input.split('\n')
 
 const dayOne = () => {
-  let copyInput = JSON.parse(JSON.stringify(input))
-  //! start by making intial paths
   
-  // for(path of copyInput) {
-  //   let [start, end] = path.split('-')
-  //   if(start == 'start') paths.push(path)
-  // }
-  // copyInput = copyInput.filter( path => !paths.includes(path))
-    
-  console.log(copyInput);
-  
-  let paths = [];
-  for(path in copyInput) {
-    let [start, end] = path.split('-')
-    let starts = [];
-    
-  }
+  console.log(paths)
 
-  console.log(paths);
 }
 
 const dayTwo = () => {
