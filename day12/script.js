a144f0943fd10034e4d980dbb2c682521b41d94f
Kevin <kevinator900@gmail.com>
2021-12-15 13:05:12 -0700

    successfully started day12

--- day12/script.js
+++ day12/script.js
@@ -30,12 +30,50 @@ A-b
 b-d
 A-end
 b-end`
-input = input.split('\n')
+input = input.split('\n').map( (dir) => dir.split('-'))
+
+const isEnded = (arr) => {
+  let ended = false;
+  arr.map( (item) => {
+    if(item[-1] !== 'end'){
+      ended = true;
+    }
+  })
+  return ended;
+}
 
 const dayOne = () => {
-  
-  console.log(paths)
+  let copyInput = JSON.parse(JSON.stringify(input))
+  //! loop through all starts, pushing the possible paths to an arr
+  //! keep looping through this arr to add the next part of the path
+  //! repeat until no other paths can be made
 
+  let paths = [];
+  //! add the starts
+  for(path of copyInput){
+    if(path[0] == 'start') paths.push([path[0], path[1]])
+  }
+  copyInput = copyInput.filter( (path) => !path.includes('start'))
+
+  let searching = true;
+  while(searching) {
+    let copyPaths = Array.from(paths)
+    //! look at paths, change copyPaths
+    for(path in paths) {
+      let nextStep = paths[path][1]
+      let changed = false;
+      copyInput.map( (dir) => {
+        if(dir.includes(nextStep) && !changed){
+          copyPaths[path].push( (dir[0] == nextStep) ? dir[1] : dir[0])
+          changed = true;
+        }
+      })
+      console.log(copyPaths);
+    }
+
+    paths = Array.from(paths)
+    searching = false
+  }
 }
 
 const dayTwo = () => {
---
+++ day15/script.js
@@ -0,0 +1,23 @@
+let input = `1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581`;
+input = input.split('\n').map( (row) => row.split(''))
+console.log(input);
+
+const dayOne = () => {
+  
+}
+
+const dayTwo = () => {
+
+}
+
+dayOne();
+dayTwo();
